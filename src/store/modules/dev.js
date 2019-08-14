3fdf662df9b4697bb650b83afa0e739e1fb464af
lee <ejsvk3284@gmail.com>
2019-08-14 13:34:43 +0900

    Fix conflict

--- src/store/modules/dev.js
+++ src/store/modules/dev.js
@@ -8,7 +8,6 @@ const SCAN_DEV = 'dev/SCAN_DEV';
 const SET_REQUEST_DEV = 'dev/SET_REQUEST_DEV';
 const CONNECT_DEV = 'dev/CONNECT_DEV';
 const SET_IS_MODAL_WITH_FALSE = 'dev/SET_IS_MODAL_WITH_FALSE';
-<<<<<<< HEAD
 const REQUEST_CONNECTED_DEVS = 'dev/REQUEST_CONNECTED_DEVS';
 const GET_DEV_DETAIL = 'dev/GET_DEV_DETAIL';
 
@@ -19,14 +18,6 @@ export const connectDev = createAction(CONNECT_DEV, DevApi.connectDev);
 export const setIsModalWithFalse = createAction(SET_IS_MODAL_WITH_FALSE);
 export const requestConnectedDevs = createAction(REQUEST_CONNECTED_DEVS, DevApi.requestConnectedDevs);
 export const getDevDetail = createAction(GET_DEV_DETAIL, DevApi.getDevDetail); 
-=======
-
-/*--------create action--------*/
-export const scanDev = createAction(SCAN_DEV, DevApi.scanDev);
-export const setRequestDev = createAction(SET_REQUEST_DEV, macAddr => macAddr);
-export const connectDev = createAction(CONNECT_DEV, DevApi.connectDev);
-export const setIsModalWithFalse = createAction(SET_IS_MODAL_WITH_FALSE);
->>>>>>> upstream/feature
 
 /*--------state definition--------*/
 const initialState = Map({
@@ -34,29 +25,19 @@ const initialState = Map({
 	dev: Map({
 
 		scanDevs: List([]),
-<<<<<<< HEAD
 	
 		connectedDevs: List([]),
 
 		requestDev: '',
 
 		devDetail: '',
-=======
-
-		requestDev: '',
-	
-		connectedDevs: List([]),
->>>>>>> upstream/feature
 	}),
 
 	isModal: false,
 
 	isResult: null,
-<<<<<<< HEAD
 
 	isLoading: false,
-=======
->>>>>>> upstream/feature
 })
 
 /*--------reducer--------*/
@@ -75,7 +56,6 @@ export default handleActions({
 		type: SCAN_DEV,
 		onSuccess: (state, action) => {
 			const devs = action.payload.data.devices;
-<<<<<<< HEAD
 			return state.setIn(['dev', 'scanDevs'], devs)
 						.set('isLoading', false);
 		},
@@ -84,9 +64,6 @@ export default handleActions({
 		},
 		onPending: (state, action) => {
 			return state.set('isLoading', true);
-=======
-			return state.setIn(['dev', 'scanDevs'], devs);
->>>>>>> upstream/feature
 		}
 	}),
 
@@ -96,7 +73,6 @@ export default handleActions({
 			const requestDev = state.getIn(['dev', 'requestDev']);
 			const scanDevs = state.getIn(['dev', 'scanDevs']);
 			const connectedDevs = state.getIn(['dev', 'connectedDevs']);
-<<<<<<< HEAD
 			return state.setIn(['dev', 'scanDevs'], scanDevs.filter((item) => item !== requestDev))
 						//.setIn(['dev', 'connectedDevs'], connectedDevs => connectedDevs.push(requestDev))
 						.setIn(['dev', 'connectedDevs'], [ ...connectedDevs, requestDev ])
@@ -104,19 +80,10 @@ export default handleActions({
 						.set('isModal', true)
 						.set('isResult', true)
 						.set('isLoading', false);
-=======
-
-			return state.setIn(['dev', 'scanDevs'], scanDevs.filter((item) => item.address !== requestDev))
-						.setIn(['dev', 'connectedDevs'], connectedDevs.push(requestDev))
-						.setIn(['dev', 'requestDev'], '')
-						.set('isModal', true)
-						.set('isResult', true);
->>>>>>> upstream/feature
 		},
 		onFailure: (state, action) => {
 			return state.setIn(['dev', 'requestDev'], '')
 						.set('isModal', true)
-<<<<<<< HEAD
 						.set('isResult', false)
 						.set('isLoading', false);
 		},
@@ -138,9 +105,6 @@ export default handleActions({
 		onSuccess: (state, action) => {
 			const devDetail = action.payload.data.devDetail;
 			return state.setIn(['dev', 'devDetail'], devDetail);
-=======
-						.set('isResult', false);
->>>>>>> upstream/feature
 		}
 	})
 }, initialState);
